bf6bfe8e74da11b97acc38e6d17d93fd158f5444
Stremiouser666 <stremiouser666@github.com>
2026-02-16 05:32:50 +0000

    Initial setup

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("com.android.application") version "7.4.2" apply false
+    id("com.android.library") version "7.4.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
