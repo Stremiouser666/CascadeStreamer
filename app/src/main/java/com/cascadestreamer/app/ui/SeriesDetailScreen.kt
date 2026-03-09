8dfb83f6bf2dd8eb58a1331ca944c75a42f6c095
Stremiouser666 <stremiouser666@github.com>
2026-03-09 05:43:37 +0000

    Add horizontalScroll import

--- app/src/main/java/com/cascadestreamer/app/ui/SeriesDetailScreen.kt
+++ app/src/main/java/com/cascadestreamer/app/ui/SeriesDetailScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
