eed40ebaa8d0d5bb17fca148b5a5184264c3023d
Stremiouser666 <stremiouser666@github.com>
2026-03-10 21:33:46 +0000

    Remove duplicate Icons import

--- app/src/main/java/com/cascadestreamer/app/ui/DescriptionPopup.kt
+++ app/src/main/java/com/cascadestreamer/app/ui/DescriptionPopup.kt
@@ -18,7 +18,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.material.icons.Icons
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
